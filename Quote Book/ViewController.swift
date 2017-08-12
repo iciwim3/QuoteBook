0564f4eb4db5340d4867f447485a478514edf2e5
SKE <appybuildmore@gmail.com>
2017-08-12 15:58:47 -0500

    1. Updated Model code. 2. Updated Storyboard label constraints to fit all of the text.

--- Quote Book/ViewController.swift
+++ Quote Book/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class QuoteBookViewController: UIViewController {
-
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var authorImage: UIImageView!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    
+    var quoteBank = QuoteBank()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        quoteLabel.text = "Tap the button for inspiration!"
+        authorImage.image = UIImage(named: quoteBank.authorImages[0])
+        authorNameLabel.text = quoteBank.authors[0]
     }
 
+    @IBAction func inspireMeButtonTapped(_ sender: Any) {
+        if authorImage.image == UIImage(named: quoteBank.authorImages[0]) && authorNameLabel.text == quoteBank.authors[0] {
+            quoteLabel.text = quoteBank.quotes[0]
+            quoteBank.currentQuoteIndex += 1
+        } else {
+            updateQuote()
+        }
+        
+        updateQuote()
+    }
+    
+    func updateQuote() {
+        let nextQuote = quoteBank.getNextQuote()
+        let quoteAuthorImage = quoteBank.getNextAuthorImage()
+        let authorNameText = quoteBank.getNextAuthorName()
+        
+        quoteLabel.text = nextQuote
+        authorImage.image = UIImage(named: quoteAuthorImage)
+        authorNameLabel.text = authorNameText
+    }
+    
 
 }
 
